e8943bbb040326668a9ec8152f5e5be122f2dba8
taauntik <devauntik@gmail.com>
2021-09-25 23:00:24 +0600

    changes the icon of the addOrRemove button

--- src/pages/components/AddOrRemoveBtn/AddOrRemoveBtn.js
+++ src/pages/components/AddOrRemoveBtn/AddOrRemoveBtn.js
@@ -3,16 +3,24 @@ import React, { PureComponent } from "react";
 // styles
 import { BtnWrapper } from "./AddOrRemoveBtn.styles";
 
+// images
+import IncrementBtn from "../../../assets/add.svg";
+import DecrementBtn from "../../../assets/minus.svg";
+
 class AddOrRemoveBtn extends PureComponent {
   render() {
     return (
       <BtnWrapper
         onClick={this.props.onClick}
         style={{
-          padding: `${this.props.verticalPadding}px ${this.props.horizontalPadding}px`,
+          padding: "10px",
         }}
       >
-        {this.props.value}
+        {this.props.value === "+" ? (
+          <img src={IncrementBtn} alt="" width="12" />
+        ) : (
+          <img src={DecrementBtn} alt="" width="12" />
+        )}
       </BtnWrapper>
     );
   }
